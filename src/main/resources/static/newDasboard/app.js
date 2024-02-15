332578176d25b94a2a18394937a0e9f367c243dd
Maciej Kasza <kaszamaciej96@gmail.com>
2024-02-15 21:47:23 +0100

    Add nav logic in js

--- src/main/resources/static/newDasboard/app.js
+++ src/main/resources/static/newDasboard/app.js
@@ -1,5 +1,23 @@
+const navCheckboxs = document.querySelectorAll(".nav-input");
+const navButtons = document.querySelectorAll(".nav-btn");
+
 function init() {
   console.log("Init...");
+  initNavigation();
 }
 
 init();
+
+function initNavigation() {
+  navButtons.forEach((button) =>
+    button.addEventListener("click", () => {
+      let checkboxID = button.getAttribute("data-checkbox");
+      let checkboxIndex = [...navCheckboxs].find(
+        (item) => item.getAttribute("data-id") === checkboxID
+      );
+      checkboxIndex.checked = !checkboxIndex.checked;
+    })
+  );
+}
+
+console.log(navCheckboxs, navButtons);
